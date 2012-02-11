e94b9af7be9ab0d27a8ab5f986ca0fce45c44623
Jordon Bedwell <jordon@envygeeks.com>
2012-02-11 12:54:43 -0600

    Fix the tabbing accidently created by the GUI editor. Signed-off-by: Jordon Bedwell <jordon@envygeeks.com>

--- lib/pry/aliases/common.rb
+++ lib/pry/aliases/common.rb
@@ -1,9 +1,9 @@
 class Pry::Plugins::VTerm
   class << self
-  	command = 'bash'
-  	if ENV['SHELL'] =~ %r!/zsh\Z!
-  		command = 'zsh'
-  	end
+    command = 'bash'
+    if ENV['SHELL'] =~ %r!/zsh\Z!
+      command = 'zsh'
+    end
 
     @@aliases = `#{command} -i -c 'alias'`.gsub(/\Aalias\s{1}/, '').split(/\salias/).inject({}) do |hash, cmd|
       cmd = cmd.split('=\'')
--- tests/pry-vterm_aliases.rb
+++ tests/pry-vterm_aliases.rb
@@ -3,14 +3,14 @@ require 'minitest/autorun'
 
 describe Pry::Plugins::VTerm do
   it 'should have a proper version number' do
-  	# We care whether we have a proper version number yes.....
+    # We care whether we have a proper version number yes.....
     Pry::Plugins::VTerm.version.split(/\./).delete_if do |val|
       val =~ /pre\d{0,2}/
     end.length.must_equal 3
   end
 
   it 'should output a hash of aliases when calling aliases' do
-  	# All we care about here is that we get a Hash
-  	assert Pry::Plugins::VTerm.aliases.class, Hash
+    # All we care about here is that we get a Hash
+    assert Pry::Plugins::VTerm.aliases.class, Hash
   end
 end
