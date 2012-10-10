2bbc68d0768447a2817ce0dc5294cbf0f5aaf1da
Jordon Bedwell <jordon@envygeeks.com>
2012-10-10 07:58:39 -0500

    Clean up the code a bit.

--- lib/pry/aliases/common.rb
+++ lib/pry/aliases/common.rb
@@ -1,21 +1,23 @@
-if defined? Pry::Plugins::VTerm
-  class Pry::Plugins::VTerm
-    class << self
-      command = "bash"
-      if ENV["SHELL"] =~ %r!/zsh\Z!
-        command = "zsh"
-      end
-
-      @@aliases = `#{command} -i -c 'alias'`.each_line.to_a.map(&:chomp).delete_if { |line|
-        line !~ /=/ }.inject({}) do |hash, cmd|
-          cmd = Shellwords.shellwords(cmd.gsub(/\Aalias\s{1}/, "")).join
-          cmd = cmd.split("=")
-          cmd[1] = cmd[1..-1].join("=")
-
-          unless cmd[0] =~ /\s/
-            hash.update({cmd[0] => cmd[1]})
-          end
+class Pry
+  class Plugins
+    class VTerm
+      class << self
+        command = "bash"
+        if ENV["SHELL"] =~ %r!/zsh\Z!
+          command = "zsh"
         end
+
+        @@aliases = `#{command} -i -c 'alias'`.each_line.to_a.map(&:chomp).delete_if { |line|
+          line !~ /=/ }.inject({}) do |hash, cmd|
+            cmd = Shellwords.shellwords(cmd.gsub(/\Aalias\s{1}/, "")).join
+            cmd = cmd.split("=")
+            cmd[1] = cmd[1..-1].join("=")
+
+            unless cmd[0] =~ /\s/
+              hash.update({cmd[0] => cmd[1]})
+            end
+          end
+      end
     end
   end
 end
