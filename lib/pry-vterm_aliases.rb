2081b1516651803a06fe63bcd39bbd0cfe330b03
Jordon Bedwell <jordon@envygeeks.com>
2012-10-10 07:50:59 -0500

    Code clean up and add VERSION to comply with coding standards.

--- lib/pry-vterm_aliases.rb
+++ lib/pry-vterm_aliases.rb
@@ -1,21 +1,22 @@
-$:.unshift File.dirname(__FILE__) and require 'pry'
+$:.unshift File.dirname(__FILE__) and require "pry"
 unless defined? Pry::Plugins
-  Pry.const_set :Plugins, Class.new
+  Pry.const_set(:Plugins, Class.new)
 end
 
-unless RbConfig::CONFIG['host_os'] =~ /mswin/
-  Pry::Commands.block_command /\.(.*)/, '.shell commands' do |cmd|
+unless RbConfig::CONFIG["host_os"] =~ /mswin|mingw32/
+  Pry::Commands.block_command /\.(.*)/, ".shell commands" do |cmd|
     if defined?(Pry::Plugins::VTerm) && Pry::Plugins::VTerm.aliases.include?(cmd)
       cmd = Pry::Plugins::VTerm.aliases[cmd]
     end
 
-    Pry.config.system.call Pry.output, cmd, _pry_
+    Pry.config.system.call(Pry.output, cmd, _pry_)
   end
 
   class Pry::Plugins::VTerm
     class << self
+      VERSION = "0.0.7"
       def version
-        '0.0.7'
+        VERSION
       end
 
       def aliases
@@ -25,6 +26,6 @@ unless RbConfig::CONFIG['host_os'] =~ /mswin/
   end
 
   case ENV['SHELL']
-    when %r!/(?:bash|zsh)\Z! then require 'pry/aliases/common'
+    when %r!/(?:bash|zsh)\Z! then require "pry/aliases/common"
   end
 end
