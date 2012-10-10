c0ca0fc4e787f1463069655fccc48390927e77e5
Jordon Bedwell <jordon@envygeeks.com>
2012-10-10 07:42:50 -0500

    Clean up the tests a bit.

--- pry-vterm_aliases.gemspec
+++ pry-vterm_aliases.gemspec
@@ -8,16 +8,7 @@ Gem::Specification.new do |spec|
   spec.authors = ["Jordon Bedwell"]
   spec.add_dependency('pry', '~> 0.9.8')
   spec.description = "Preserve your VTerm aliases in Pry."
-  spec.files = [
-    "lib/pry",
-    "readme.md",
-    "license.txt",
-    "lib/pry/aliases",
-    "lib/pry/aliases/common.rb",
-    "lib/pry-vterm_aliases.rb",
-    "rakefile.rb",
-    "gemfile.rb"
-  ]
+  spec.files = Dir["**/*"]
   spec.homepage = 'https://github.com/envygeeks/pry-vterm_aliases'
   spec.licenses = ["MIT"]
   spec.require_paths = ["lib"]
--- tests/pry-vterm_aliases.rb
+++ tests/pry-vterm_aliases.rb
@@ -1,16 +1,13 @@
-require './lib/pry-vterm_aliases'
-require 'minitest/autorun'
+require "./lib/pry-vterm_aliases"
+require "minitest/autorun"
 
 describe Pry::Plugins::VTerm do
-  it 'should have a proper version number' do
-    # We care whether we have a proper version number yes.....
-    Pry::Plugins::VTerm.version.split(/\./).delete_if do |val|
-      val =~ /pre\d{0,2}/
-    end.length.must_equal 3
+  it "should have a proper version number" do
+    Pry::Plugins::VTerm.version.split(/\./).delete_if { |val|
+      val =~ /pre\d{0,2}/ }.length.must_equal 3
   end
 
-  it 'should output a hash of aliases when calling aliases' do
-    # All we care about here is that we get a Hash
+  it "should output a hash of aliases when calling aliases" do
     assert Pry::Plugins::VTerm.aliases.class, Hash
   end
 end
