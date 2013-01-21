e8847c1a92151967c894f0d2aa48232de9d1fdc1
Jordon Bedwell <envygeeks@gmail.com>
2013-01-21 10:22:16 -0600

    Code cleanup.

--- lib/pry-vterm_aliases.rb
+++ lib/pry-vterm_aliases.rb
@@ -6,16 +6,16 @@ unless ::RbConfig::CONFIG["host_os"] =~ /mswin|mingw32/
     module VTermAliases
       class << self
         def create_aliases
-          aliases.each { |k, v|
-            Pry::Commands.create_command(cmd_regexp(k), listing: ".#{k}") {
-              description "alias to: #{Pry::Helpers::Text.bold(v)}."
-              group "Terminal Aliases"
+          aliases.each do |k, v|
+            Pry::Commands.create_command(cmd_regexp(k), listing: ".#{k}") do
+              description("alias to: #{Pry::Helpers::Text.bold(v)}.")
+              group("Terminal Aliases")
 
               def process(cmd, extra)
                 Pry::VTermAliases.run_command(cmd, extra, output)
               end
-            }
-          }
+            end
+          end
         end
 
         def aliases
@@ -23,15 +23,17 @@ unless ::RbConfig::CONFIG["host_os"] =~ /mswin|mingw32/
             if shell.nil? || shell.empty?
               {}
             else
-              `#{shell} -i -c 'alias'`.split(/\n/).inject({}) { |h, (a)|
-                a = a.sub(/^alias\s/, "").split("=")
-                unless a.first =~ /\s/
-                  strip_wrapping_quotes(a.shift).tap do |key|
-                    h.update(key => Shellwords.shellwords(a.join("=")).join)
+              `#{shell} -ic 'alias'`.split(/\n/).inject({}) do |hash, (alius)|
+                alius = alius.sub(/^alias\s/, "").split("=")
+                unless alius.first =~ /\s/
+                  strip_wrapping_quotes(alius.shift).tap do |key|
+                    hash.update(
+                      key => Shellwords.shellwords(alius.join("=")).join
+                    )
                   end
                 end
-              h
-              }
+              hash
+              end
           end
         end
 
@@ -41,11 +43,6 @@ unless ::RbConfig::CONFIG["host_os"] =~ /mswin|mingw32/
 
         def run_command(cmd, extra, output)
           raise ArgumentError, "unknown alias" unless (cmd = aliases[cmd])
-
-          unless extra.nil? || extra.empty?
-            extra = " " + extra.sub(/^\s+/, "")
-          end
-
           output.puts(`#{cmd}#{extra}`)
           $?.success?
         end
@@ -66,12 +63,12 @@ unless ::RbConfig::CONFIG["host_os"] =~ /mswin|mingw32/
         private
         def capture_vterm_alias(alius, extra = nil, out = StringIO.new)
           Pry::VTermAliases.run_command(alius.gsub(/^\./, ""), extra, out)
-          StringIO === out ? out.string : out
+          ((StringIO === out) ? (out.string) : (out))
         end
       end
     end
   end
 
-  ::Pry::VTermAliases.create_aliases
-  ::Object.send(:include, Pry::VTermAliases::ObjectExt)
+  Pry::VTermAliases.create_aliases
+  Object.send(:include, Pry::VTermAliases::ObjectExt)
 end
