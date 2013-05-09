c8aa4c403a60ba136a6632550852b07b40c06f3d
Jordon Bedwell <envygeeks@gmail.com>
2013-05-08 21:45:58 -0500

    Cleanup specs a bit.

--- spec/lib/pry-vterm_aliases_spec.rb
+++ spec/lib/pry-vterm_aliases_spec.rb
@@ -12,28 +12,26 @@ describe Pry::VTermAliases do
     end
   end
 
-  context "when a user has no shell defined" do
-    it "should return a blank hash for aliases" do
-      @aliases = subject.send(:remove_instance_variable, :@aliases)
-      @shell = subject.instance_variable_get(:@shell)
-      subject.instance_variable_set(:@shell, "")
+  it "returns a blank hash for aliases if no shell is defined" do
+    @aliases = subject.send(:remove_instance_variable, :@aliases)
+    @shell = subject.instance_variable_get(:@shell)
+    subject.instance_variable_set(:@shell, "")
 
-      expect(subject.aliases).to be_instance_of Hash
+    expect(subject.aliases).to be_instance_of Hash
 
-      subject.instance_variable_set(:@shell, @shell)
-      subject.instance_variable_set(:@aliases, @aliases)
-    end
+    subject.instance_variable_set(:@shell, @shell)
+    subject.instance_variable_set(:@aliases, @aliases)
   end
 
-  it "it should give a list of aliases if asked" do
+  it "gives a list of aliases if asked" do
     expect(subject.aliases.count).to be > 0
   end
 
-  it "should raise if an alias is unknown" do
+  it "raises if an alias is unknown" do
     expect { subject.run_alias("hello", "") }.to raise_error ArgumentError
   end
 
-  it "should run aliases as asked" do
+  it "runs aliases as asked" do
     stub_grave_commands
     expect(
       subject.run_alias("ls", "Gemfile", out = StringIO.new)).to eq true
